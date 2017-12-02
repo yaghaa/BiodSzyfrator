97cb348506fb0fd5f734ec4b938a044551545144
yaghaa <za.rogiem@gmail.com>
2017-12-02 23:13:24 +0100

    trdt

---
+++ BiodSzyfrator/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace BiodSzyfrator
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.tbSourceFile = new System.Windows.Forms.TextBox();
+            this.tbOutcome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bChooseBaseFile = new System.Windows.Forms.Button();
+            this.bOutput = new System.Windows.Forms.Button();
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bEncrypt = new System.Windows.Forms.Button();
+            this.bDecrypt = new System.Windows.Forms.Button();
+            this.bInfo = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // tbSourceFile
+            // 
+            this.tbSourceFile.Location = new System.Drawing.Point(27, 24);
+            this.tbSourceFile.Name = "tbSourceFile";
+            this.tbSourceFile.ReadOnly = true;
+            this.tbSourceFile.Size = new System.Drawing.Size(358, 20);
+            this.tbSourceFile.TabIndex = 0;
+            // 
+            // tbOutcome
+            // 
+            this.tbOutcome.Location = new System.Drawing.Point(27, 129);
+            this.tbOutcome.Name = "tbOutcome";
+            this.tbOutcome.ReadOnly = true;
+            this.tbOutcome.Size = new System.Drawing.Size(358, 20);
+            this.tbOutcome.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ścieżka pliku wyjściowego";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ścieżka pliku wejsciowego";
+            // 
+            // bChooseBaseFile
+            // 
+            this.bChooseBaseFile.Location = new System.Drawing.Point(392, 22);
+            this.bChooseBaseFile.Name = "bChooseBaseFile";
+            this.bChooseBaseFile.Size = new System.Drawing.Size(122, 23);
+            this.bChooseBaseFile.TabIndex = 4;
+            this.bChooseBaseFile.Text = "Wczytaj plik";
+            this.bChooseBaseFile.UseVisualStyleBackColor = true;
+            this.bChooseBaseFile.Click += new System.EventHandler(this.bChooseBaseFile_Click);
+            // 
+            // bOutput
+            // 
+            this.bOutput.Location = new System.Drawing.Point(392, 127);
+            this.bOutput.Name = "bOutput";
+            this.bOutput.Size = new System.Drawing.Size(122, 23);
+            this.bOutput.TabIndex = 5;
+            this.bOutput.Text = "Wybierz ścieżkę";
+            this.bOutput.UseVisualStyleBackColor = true;
+            this.bOutput.Click += new System.EventHandler(this.bOutput_Click);
+            // 
+            // tbKey
+            // 
+            this.tbKey.Location = new System.Drawing.Point(27, 77);
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(358, 20);
+            this.tbKey.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Klucz szyfrujący/deszyfrujący";
+            // 
+            // bEncrypt
+            // 
+            this.bEncrypt.BackColor = System.Drawing.Color.MediumPurple;
+            this.bEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bEncrypt.Location = new System.Drawing.Point(27, 188);
+            this.bEncrypt.Name = "bEncrypt";
+            this.bEncrypt.Size = new System.Drawing.Size(173, 51);
+            this.bEncrypt.TabIndex = 8;
+            this.bEncrypt.Text = "Szyfruj";
+            this.bEncrypt.UseVisualStyleBackColor = false;
+            this.bEncrypt.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // bDecrypt
+            // 
+            this.bDecrypt.BackColor = System.Drawing.Color.MediumPurple;
+            this.bDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bDecrypt.Location = new System.Drawing.Point(212, 188);
+            this.bDecrypt.Name = "bDecrypt";
+            this.bDecrypt.Size = new System.Drawing.Size(173, 51);
+            this.bDecrypt.TabIndex = 9;
+            this.bDecrypt.Text = "Deszyfruj";
+            this.bDecrypt.UseVisualStyleBackColor = false;
+            // 
+            // bInfo
+            // 
+            this.bInfo.Location = new System.Drawing.Point(392, 216);
+            this.bInfo.Name = "bInfo";
+            this.bInfo.Size = new System.Drawing.Size(122, 23);
+            this.bInfo.TabIndex = 10;
+            this.bInfo.Text = "Info";
+            this.bInfo.UseVisualStyleBackColor = true;
+            this.bInfo.Click += new System.EventHandler(this.bInfo_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "ofdInFIle";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "ofdOutFile";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(535, 264);
+            this.Controls.Add(this.bInfo);
+            this.Controls.Add(this.bDecrypt);
+            this.Controls.Add(this.bEncrypt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbKey);
+            this.Controls.Add(this.bOutput);
+            this.Controls.Add(this.bChooseBaseFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbOutcome);
+            this.Controls.Add(this.tbSourceFile);
+            this.Name = "Form1";
+            this.Text = "Szyfrator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox tbSourceFile;
+    private System.Windows.Forms.TextBox tbOutcome;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Button bChooseBaseFile;
+    private System.Windows.Forms.Button bOutput;
+    private System.Windows.Forms.TextBox tbKey;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.Button bEncrypt;
+    private System.Windows.Forms.Button bDecrypt;
+    private System.Windows.Forms.Button bInfo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ BiodSzyfrator/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BiodSzyfrator
+{
+    public partial class Form1 : Form
+    {
+        private string _myKey = "";
+        // klucz zagwiazgkowany !!!!
+        // klucz usuwamy z ekranu po wybraniu 'szyfruj' 'deszyfruj'
+        //na pulpicie katalog
+        //- 2 sensowne pliki o tresci jawnej
+        //- plikacja wykonywalna, kod źródłowy
+        public Form1()
+        {
+            InitializeComponent();
+            tbKey.PasswordChar = '*';
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbKey.Text))
+            {
+                MessageBox.Show("Podaj klucz!");
+            }
+            else if (IsKeyValid(tbKey.Text) == false)
+            {
+                
+            }
+            else
+            {
+                _myKey = tbKey.Text;
+                tbKey.Text = "";
+                var file = ReadFile();
+                var result = Encrypt(file);
+                SaveFile(PrepareText(result));
+            }
+        }
+
+        private bool IsKeyValid(string myKey)
+        {
+            if (myKey.Length < 10)
+            {
+                MessageBox.Show("Klucz musi posiadać przynajmniej 10 znaków. Popraw klucz.");
+                return false;
+            }
+            var numbersInKey = myKey.Where(x => (int) x >= 48 && (int) x <= 57).ToList();
+            if (numbersInKey.Count < 4)
+            {
+                MessageBox.Show("Klucz musi posiadać przynajmniej 4 cyfry. Popraw klucz.");
+                return false;
+            }
+            var letersInKey = myKey.Where(x => ((int)x >= 65 && (int)x <= 90) || ((int)x >= 97 && (int)x <= 122)).ToList();
+            if (letersInKey.Count < 4)
+            {
+                MessageBox.Show("Klucz musi posiadać przynajmniej 4 litery. Popraw klucz.");
+                return false;
+            }
+            var specialInKey = myKey.Where(x => ((int)x >= 33 && (int)x <= 47)).ToList();
+            if (specialInKey.Count < 2)
+            {
+                MessageBox.Show("Klucz musi posiadać przynajmniej 2 znaki specjalne. Popraw klucz.");
+                return false;
+            }
+            return true;
+        }
+
+        private string Encrypt(string file)
+        {
+            string[] blocks = GetBlocks(file);
+
+            string[] substitutedBlocks = Substitute(blocks);
+
+            string[] movedBlocks = Displacement(substitutedBlocks);
+
+            return String.Join("", movedBlocks);
+        }
+
+        private string[] Displacement(string[] substitutedBlocks)
+        {
+            for (int i = 0; i < substitutedBlocks.Count(); i++)
+            {
+                substitutedBlocks[i] = GetDisplacementBlock(substitutedBlocks[i]);
+            }
+
+            return substitutedBlocks;
+        }
+
+        private string GetDisplacementBlock(string substitutedBlock)
+        {
+            var displacementTable = new char[5, 5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (PyramidFieldReserved(i,j) == false)
+                    {
+                        displacementTable[i][j] =
+                    }
+                    else
+                    {
+                        displacementTable[i][j] = null;
+                    }
+                }
+            }
+
+            displacementTable[0, 4] = substitutedBlock[0];
+            displacementTable[1, 2] = substitutedBlock[1];
+            displacementTable[1, 3] = substitutedBlock[2];
+            displacementTable[1, 4] = substitutedBlock[3];
+            displacementTable[2, 0] = substitutedBlock[4];
+            displacementTable[2, 1] = substitutedBlock[5];
+            displacementTable[2, 2] = substitutedBlock[6];
+            displacementTable[2, 3] = substitutedBlock[7];
+            displacementTable[2, 4] = substitutedBlock[8];
+            displacementTable[3, 2] = substitutedBlock[9];
+            displacementTable[3, 3] = substitutedBlock[10];
+            displacementTable[3, 4] = substitutedBlock[11];
+            displacementTable[4, 4] = substitutedBlock[12];
+
+            var newBlock = displacementTable[2, 0].ToString() +
+                           displacementTable[2, 1].ToString() +
+                           displacementTable[1, 2].ToString() +
+                           displacementTable[2, 2].ToString() +
+                           displacementTable[3, 2].ToString() +
+                           displacementTable[2, 3].ToString() +
+                           displacementTable[1, 3].ToString() +
+                           displacementTable[3, 3].ToString() +
+                           displacementTable[0, 4].ToString() +
+                           displacementTable[1, 4].ToString() +
+                           displacementTable[2, 4].ToString() +
+                           displacementTable[3, 4].ToString() +
+                           displacementTable[4, 4].ToString();
+            return newBlock;
+        }
+
+        private bool PyramidFieldReserved(int i, int j)
+        {
+            if ((i == 1 && j == 1) || (i == 1 && j == 1))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private string[] Substitute(string[] blocks)
+        {
+            var moveIndex = GetMoveIndex();
+            for (int i = 0; i < blocks.Count(); i++)
+            {
+                var newBlock = "";
+                for (int j = 0; j < blocks[i].Count(); j++)
+                {
+                    var newChar = GetNewChar(blocks[i][j], moveIndex + j);
+                    newBlock += newChar;
+                }
+                blocks[i] = newBlock;
+            }
+
+            return blocks;
+        }
+
+        private char GetNewChar(char c, int i)
+        {
+            var number = (int)c;
+            number += i;
+            return (char)number;
+        }
+
+        private int GetMoveIndex()
+        {
+            var count = _myKey.Count(Char.IsDigit);
+            count += _myKey.Where(Char.IsDigit).Sum(p => Convert.ToInt32(p.ToString()));
+            return count;
+        }
+
+        private string[] GetBlocks(string file)
+        {
+            var additionalRow = 0;
+            if (file.Length % 13 != 0)
+            {
+                additionalRow = 1;
+            }
+            var blockCount = (file.Length / 13 + additionalRow);
+            string[] blocks = new string[blockCount];
+            var numberInBlock = 0;
+            var blockNumber = 0;
+            var block = "";
+
+            for (int j = 0; j < file.Count(); j++)
+            {
+                if (numberInBlock < 13)
+                {
+                    block = block + file[j];
+                    if (j == file.Count() - 1)
+                    {
+                        for (int i = numberInBlock; i < 13; i++)
+                        {
+                            block = block + (char)32;
+                        }
+                        blocks[blockNumber] = block;
+                        break;
+                    }
+                    numberInBlock++;
+                }
+                else
+                {
+                    blocks[blockNumber] = block;
+                    block = "";
+                    block = block + file[j];
+
+                    blockNumber++;
+                    numberInBlock = 1;
+                }
+            }
+            return blocks;
+        }
+
+        private string PrepareText(string toString)
+        {
+            return toString;
+        }
+
+        private string ReadFile()
+        {
+            return String.Join(((char)13).ToString(), System.IO.File.ReadAllLines(tbSourceFile.Text, Encoding.GetEncoding(1250)));
+        }
+
+        private void SaveFile(string text)
+        {
+            System.IO.File.WriteAllText(tbOutcome.Text, text, Encoding.GetEncoding(1250));
+        }
+
+        private void bChooseBaseFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "test";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                tbSourceFile.Text = openFileDialog1.FileName.ToString();
+            }
+        }
+
+        private void bOutput_Click(object sender, EventArgs e)
+        {
+            openFileDialog2.FileName = "test";
+            DialogResult result = openFileDialog2.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                tbOutcome.Text = openFileDialog2.FileName.ToString();
+            }
+        }
+
+        private void bInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Klucz szyfrujący musi:"
+                            + Environment.NewLine + "• zawierać co najmniej 10"
+                            + Environment.NewLine + "• zawierać co najmniej 4 cyfry"
+                            + Environment.NewLine + "• zawierać co najmniej 4 litery"
+                            + Environment.NewLine + "• zawierać co najmniej 2 znaki specjalne"
+                            + Environment.NewLine
+                            + Environment.NewLine + "Gdy nie wybierzesz ścieżki docelowej Szyfrogram zapisze zaszyfrowany plik"
+                            + Environment.NewLine + " w tym samym katalogu co plik źródłowy, pod nazwą 'encrypted.txt'"
+            );
+        }
+    }
+}
